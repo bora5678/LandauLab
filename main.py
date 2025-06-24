f8e3bbf2262c3851bbf525591b902e366dd1b843
NANOLAB TUEINAN <ga84veh@mytum.de>
2025-06-24 17:46:44 +0200

    Experiment was great success. Added plotting. Remember to check out key33B errors

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+from pickle import FALSE
 from sample.Experiment import find_initial_frequency, measurement
+import matplotlib.pyplot as plt
+import numpy as np
 
+def dbm_to_vrms(dbm, R=50):
+
+    """Convert dBm to RMS voltage"""
+    dbm = np.array(dbm)
+    amp_power_watts = 10 ** (dbm / 10) * 1e-3
+    amp_voltage_rms = np.sqrt(amp_power_watts * R)
+    return amp_voltage_rms
 
 if __name__ == '__main__':
     """Main function that runs everything."""
-    i = find_initial_frequency()
+    i = find_initial_frequency() 
+    f, amp, freq = measurement()
+
+    amp_lin = dbm_to_vrms(amp, R=50)
+
+    print(f'Amplitudes vector: {amp_lin}')
+    print(f'Freq vector: {freq}')
     print(f"Initial frequency: {i} Hz")
-    f = measurement()
     print(f"Final frequency: {f} Hz")
+
+    vlt = np.linspace(-20, -12, len(amp_lin))
+    plt.scatter(vlt, amp_lin, color='blue', marker='o')                  
+    plt.xlabel("Voltage")
+    plt.ylabel("Amplitude(Vrms)")
+    plt.title("Mechanical Mode")
+    plt.grid(False)
+    plt.show()
--- sample/Experiment.py
+++ sample/Experiment.py
@@ -9,50 +9,74 @@ def find_initial_frequency() -> float:
     """Find the starting point of the measurement."""
     gs200 = Yokogawa()
     gs200.set_range(30)
-    gs200.set_voltage(-18)
+    gs200.set_voltage(-20)
     time.sleep(1)  # Allow some time for the voltage to stabilize
 
     ks = Keysight33500B()
-    ks.selectSine(unit="VPP", amp=2.0, freq=6.6647e6, offset=0.0)
+    ks.selectSine(unit="VPP", amp=0.1, freq=6.6647e6, offset=0.0)
     ks.outp_on(channel=1)
     time.sleep(1)  # Allow some time for the signal to stabilize
 
     fsv = FSV()
-    fsv.span(1e6)  # Set span to 20 MHz
-    fsv.bw(10, 2001, 1.0, 1)  # Set bandwidth to 10, sweep points to 2001
+    fsv.span(1e6)  
+    fsv.bw(10, 2001, 1.0, 1)  
     fsv.fsv_sweeptype(0.0)
-    fsv.configMaxAvg(2001, 7e6, 1e6, 1)  # Configure for measurement
+    fsv.configMaxAvg(2001, 7e6, 1e6, 1)  
 
-    a, f = fsv.scan(2001)  # Perform a scan with 2001 points
+    a, f = fsv.scan(2001)  
 
     pos = np.where(a == np.max(a))
+
+    print(f)
+
     return f[pos[0][0]]
 
 def measurement():
     """Perform the measurement."""
+
+    amp = []
+    freq = []
+
+    a_peak = []
+    f_peak = []
+
+ 
+
     gs200 = Yokogawa()
     gs200.set_range(30)
-    gs200.set_voltage(-18)
+    gs200.set_voltage(-20)
 
     key = Keysight33500B()
-    key.selectSine(unit="VPP", amp=2.0, freq=6.6647e6, offset=0.0)
+    key.selectSine(unit="VPP", amp=0.1, freq=6.6647e6, offset=0.0)
     key.outp_on(channel=1)
     time.sleep(1)  # Allow some time for the signal to stabilize
 
     tau = 1
 
-    for i in range(-18, -12, 0.1):
+    for i in np.arange(-20, -12, 0.1):
         gs200.set_voltage(i)
+
+        fsv = FSV()
+        fsv.span(1e6)  
+        fsv.bw(10, 2001, 1.0, 1)  
+        fsv.fsv_sweeptype(0.0)
+        fsv.configMaxAvg(2001, 7e6, 1e6, 1)  
+        amp, freq = fsv.scan(2001)  
+
+        a_peak.append(np.max(amp))
+        pos = np.where(amp == np.max(amp))
+        f_peak.append(freq[pos[0][0]])
+   
         time.sleep(tau)
 
     fsv = FSV()
-    fsv.span(20e6)  # Set span to 20 MHz
-    fsv.bw(0.2e6, 2001, 1.0, 1)  # Set bandwidth to 0.2 MHz, sweep points to 2001
+    fsv.span(1e6)  # Set span to 20 MHz
+    fsv.bw(10, 2001, 1.0, 1)  # Set bandwidth to 0.2 MHz, sweep points to 2001
     fsv.fsv_sweeptype(0.0)
     fsv.configMaxAvg(2001, 7e6, 1e6, 1)  # Configure for measurement
   
     a,f = fsv.scan(2001)  # Perform a scan with 2001 points
     pos = np.where(a == np.max(a))
     freq_final = f[pos[0][0]]
-
-    return freq_final
+    print(f)
+    return freq_final, a_peak, f_peak
